3d8f57df923aaaa71933fb7b9e52bf586d80206c
BabekBaratov <mamashaniko01@gmail.com>
2020-01-17 18:50:36 +0200

    Создание репозитория

---
+++ Game-MadCrody/Game-MadCrody.cpp
@@ -0,0 +1,12 @@
+﻿// Game-MadCrody.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "game.h"
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	game game;
+	game.menu();
+
+	system("pause");
+}
